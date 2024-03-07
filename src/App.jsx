39bebd6df9d0125daa67503d31770a20c6a73447
BeatrizRodriguezReglero <beatriz.rreglero@gmail.com>
2024-03-07 11:42:39 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import MainContainer from "./components/main-container/MainContainer";
+import Home from "./pages/Home";
+import Router from "./router/Router";
+import { GlobalStyles } from "./styles/GlobalStyles";
+
 const App = () => {
-	return <h1>Núcleo de la aplicación</h1>;
+	return (
+		<>
+		<GlobalStyles/>
+		<MainContainer/>
+		<Home/>
+		<Router/>
+		</>
+	)
+	
 };
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,6 @@
+import MainContainer from "../components/main-container/MainContainer"
+
+const Home=()=>{
+    return <MainContainer/>
+}
+export default Home
